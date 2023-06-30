b6d67f937d31f7bf8418b1328a5e5b59921523b6
Mike Partelow <github@mikepartelow.com>
2023-06-29 18:55:25 -0700

    what i really meant

--- pkg/logging/logging.go
+++ pkg/logging/logging.go
@@ -4,7 +4,7 @@ import (
 	"io"
 	"os"
 
-	"github.com/mikepartelow/logzo/pkg/logging/tater"
+	"github.com/mikepartelow/logzo/pkg/tater"
 	"golang.org/x/exp/slog"
 )
 
--- pkg/tater/tater_test.go
+++ pkg/tater/tater_test.go
@@ -2,8 +2,13 @@ package tater_test
 
 import (
 	"io"
+	"testing"
 
 	"github.com/mikepartelow/logzo/pkg/logging/tater"
 )
 
 var _ io.Writer = &tater.Tater{}
+
+func Test(t *testing.T) {
+	t.Errorf("FIXME")
+}
